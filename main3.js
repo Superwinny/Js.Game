c729e278b2319040ad283c525a89a74756863fd7
Dos Santos Davide <dos.davide91@gmail.com>
2023-06-02 17:08:45 +0200

    commit

--- main3.js
+++ main3.js
@@ -48,11 +48,8 @@ const acheter = () => {
 }
 
 
-
-
-
 function jouerPartie() {
-  const hero = new Guerrier();
+  const hero = Hero.getHeroFromLocalStorage();
   const marchand = new Marchand();
   let phase = 1;
   let enemyIndex = 0;
@@ -78,12 +75,25 @@ function jouerPartie() {
     new ChevalierCorrompu()
   ];
 
+  // Fonction pour l'attaque de l'ennemi
+  const enemyAttaque = (enemy, hero) => {
+    hero.ptnVie -= enemy.degats;
+    console.log(`${enemy.constructor.name} attaque ${hero.constructor.name} !`);
+    console.log(`Dégâts infligés: ${enemy.degats}`);
+    console.log(`Points de vie restants: ${hero.ptnVie}`);
+
+    if (hero.ptnVie <= 0) {
+      console.log(`${hero.constructor.name} est mort ! Partie terminée.`);
+      return;
+    }
+  };
+
   // Fonction pour attaquer
   const attaquer = () => {
     const currentEnemy = enemyList[enemyIndex];
 
     if (currentEnemy && currentEnemy.ptnVie > 0) {
-      currentEnemy.ptnVie -= hero.degats;
+      hero.attaquer(currentEnemy); // Utiliser la méthode attaquer() de la classe Hero
       console.log(`Vous attaquez ${currentEnemy.constructor.name} !`);
       console.log(`Dégâts infligés: ${hero.degats}`);
       console.log(`Points de vie restants: ${currentEnemy.ptnVie}`);
@@ -98,24 +108,17 @@ function jouerPartie() {
         const nextEnemy = enemyList[enemyIndex];
         console.log(`Prochain ennemi: ${nextEnemy.constructor.name}`);
         nextEnemy.afficher(pvEnemyElement, degatsEnemyElement, armeEnemyElement, faiblesseEnemyElement);
+
+        // Vérifier si le nombre d'ennemis vaincus est un multiple de 5
+        if (enemyIndex > 0 && enemyIndex % 5 === 0) {
+          console.log("Le marchand apparaît !");
+          marchand.apparaitre();
+        }
       } else {
         enemyAttaque(currentEnemy, hero);
       }
     } else {
       console.log("Il n'y a pas d'ennemi à attaquer !");
-    }
-  };
-
-  // Fonction pour l'attaque de l'ennemi
-  const enemyAttaque = (enemy, hero) => {
-    hero.ptnVie -= enemy.degats;
-    console.log(`${enemy.constructor.name} attaque ${hero.constructor.name} !`);
-    console.log(`Dégâts infligés: ${enemy.degats}`);
-    console.log(`Points de vie restants: ${hero.ptnVie}`);
-
-    if (hero.ptnVie <= 0) {
-      console.log(`${hero.constructor.name} est mort ! Partie terminée.`);
-      return;
     }
   };
 
