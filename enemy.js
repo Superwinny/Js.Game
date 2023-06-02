5dc66eaac66aeb9379d266d489d4a56345fc6f17
Dos Santos Davide <dos.davide91@gmail.com>
2023-06-02 17:01:55 +0200

    commit

--- enemy.js
+++ enemy.js
@@ -51,61 +51,281 @@ class Enemy {
  
  }
  
-     attaquer() {
-    //    let degats = Math.floor(Math.random() * 10) + 1;
-     
-     }
- 
-     
- }  
-      
-   
- 
- 
- /**********************************************************/
- /******** Guerrier **************/
- /**********************************************************/
- 
- class Guerrier extends Enemy {
-     constructor() {
-       super();
-       
-     }
-   
- 
-     attaquer() {
-    
-     }
-   }
- 
- 
- /**********************************************************/
- /******** Mage **************/
- /**********************************************************/
- 
- class Mage extends Enemy {
-     constructor() {
-       super();
-       
-     }
- 
-     attaquer() {
-    
-     }
-   }
- 
+  // Méthode pour attaquer
+  attaquer(cible) {
+    // Obtenir les dégâts de l'ennemi
+    const degats = this.degats;
+    // Infliger les dégâts à la cible
+    cible.subirDegats(degats);
+    // Vérifier si la cible est morte
+    if (cible.ptnVie <= 0) {
+      console.log(`${cible.constructor.name} est mort.`);
+      return;
+    }
+    // Obtenir les dégâts de la cible
+    const degatsCible = cible.degats;
+
+    // Infliger les dégâts à l'ennemi
+    this.subirDegats(degatsCible);
+
+    // Vérifier si l'ennemi est mort
+    if (this.ptnVie <= 0) {
+      console.log(`${this.constructor.name} est mort.`);
+      return;
+    }
+    // Répéter l'attaque réciproque
+    this.attaquer(cible);
+  }
+  // Méthode pour subir des dégâts
+  subirDegats(degats) {
+    this.#ptnVie -= degats;
+  }
+
+}
  
  /**********************************************************/
- /******** Chasseur **************/
- /**********************************************************/
- 
- class Chasseur extends Enemy {
-     constructor() {
-       super();
-       
-     }
- 
-     attaquer() {
-      
-     }
-   }
+/******** Loup **************/
+/**********************************************************/
+class Loup extends Enemy {
+    constructor() {
+      super(20, "img/imEnemy/Loup.svg", "Croc", "magic", 5);
+    }
+  
+    attaquer(cible) {
+      super.attaquer(cible);
+    }
+  }
+  
+  /**********************************************************/
+  /******** ZombieAffameur **************/
+  /**********************************************************/
+  class ZombieAffamer extends Enemy {
+    constructor() {
+      super(40, "img/imEnemy/ZombieAffamer.svg", "Griffes", "tranchant", 10);
+    }
+  
+    attaquer(cible) {
+      super.attaquer(cible);
+    }
+  }
+  
+  /**********************************************************/
+  /******** Zombie **************/
+  /**********************************************************/
+  class Zombie extends Enemy {
+    constructor() {
+      super(35, "img/imEnemy/Zombie.svg", "Mains", "trnahcant", 8);
+    }
+  
+    attaquer(cible) {
+      super.attaquer(cible);
+    }
+  }
+  
+  /**********************************************************/
+  /******** Squelette **************/
+  /**********************************************************/
+  class Squelette extends Enemy {
+    constructor() {
+      super(25, "img/imEnemy/Squelette.svg", "Épée", "critique", 6);
+    }
+  
+    attaquer(cible) {
+      super.attaquer(cible);
+    }
+  }
+  
+  /**********************************************************/
+  /******** PetitDragon **************/
+  /**********************************************************/
+  class PetitDragon extends Enemy {
+    constructor() {
+      super(50, "img/imEnemy/PetitDragon.svg", "Flammes", "Glace", 12);
+    }
+  
+    attaquer(cible) {
+      super.attaquer(cible);
+    }
+  }
+  
+  /**********************************************************/
+  /******** Ours **************/
+  /**********************************************************/
+  class Ours extends Enemy {
+    constructor() {
+      super(45, "img/imEnemy/Ours.svg", "Griffes", "Feu", 10);
+    }
+  
+    attaquer(cible) {
+      super.attaquer(cible);
+    }
+  }
+  
+  /**********************************************************/
+  /******** Minotor **************/
+  /**********************************************************/
+  class Minotor extends Enemy {
+    constructor() {
+      super(55, "img/imEnemy/Minotor.svg", "Hache", "critique", 14);
+    }
+  
+    attaquer(cible) {
+      super.attaquer(cible);
+    }
+  }
+  
+  /**********************************************************/
+  /******** MasterDragon **************/
+  /**********************************************************/
+  class MasterDragon extends Enemy {
+    constructor() {
+      super(100, "img/imEnemy/MasterDragon.svg", "Feu", "Glace", 25);
+    }
+  
+    attaquer(cible) {
+      super.attaquer(cible);
+    }
+  }
+  
+  /**********************************************************/
+  /******** Hydre **************/
+  /**********************************************************/
+  class Hydre extends Enemy {
+    constructor() {
+      super(75, "img/imEnemy/Hydre.svg", "Crocs venimeux", "Feu", 16);
+    }
+  
+    attaquer(cible) {
+      super.attaquer(cible);
+    }
+  }
+  
+  /**********************************************************/
+  /******** Guerrier Orc **************/
+  /**********************************************************/
+  class GuerrierOrc extends Enemy {
+    constructor() {
+      super(55, "img/imEnemy/GuerrierOrc.svg", "Hache", "critique", 12);
+    }
+  
+    attaquer(cible) {
+      super.attaquer(cible);
+    }
+  }
+  
+  /**********************************************************/
+  /******** Gros Zombie **************/
+  /**********************************************************/
+  class GrosZombie extends Enemy {
+    constructor() {
+      super(60, "img/imEnemy/GrosZombie.svg", "Mains", "tranchant", 14);
+    }
+  
+    attaquer(cible) {
+      super.attaquer(cible);
+    }
+  }
+  
+  /**********************************************************/
+  /******** Gorille **************/
+  /**********************************************************/
+  class Gorille extends Enemy {
+    constructor() {
+      super(50, "img/imEnemy/Gorille.svg", "Poings", "critique", 12);
+    }
+  
+    attaquer(cible) {
+      super.attaquer(cible);
+    }
+  }
+  
+  /**********************************************************/
+  /******** Golem **************/
+  /**********************************************************/
+  class Golem extends Enemy {
+    constructor() {
+      super(65, "img/imEnemy/Golem.svg", "Massue", "magic", 15);
+    }
+  
+    attaquer(cible) {
+      super.attaquer(cible);
+    }
+  }
+  
+  /**********************************************************/
+  /******** Fantôme **************/
+  /**********************************************************/
+  class Fantome extends Enemy {
+    constructor() {
+      super(30, "img/imEnemy/Fantome.svg", "Ectoplasme", "magic", 8);
+    }
+  
+    attaquer(cible) {
+      super.attaquer(cible);
+    }
+  }
+  
+  /**********************************************************/
+  /******** Dragon Rouge **************/
+  /**********************************************************/
+  class DragonRouge extends Enemy {
+    constructor() {
+      super(90, "img/imEnemy/DragonRouge.svg", "Souffle de feu", "critique", 20);
+    }
+  
+    attaquer(cible) {
+      super.attaquer(cible);
+    }
+  }
+  
+  /**********************************************************/
+  /******** Dragon Bleu **************/
+  /**********************************************************/
+  class DragonBleu extends Enemy {
+    constructor() {
+      super(85, "img/imEnemy/DragonBleu.svg", "Souffle de glace", "critique", 18);
+    }
+  
+    attaquer(cible) {
+      super.attaquer(cible);
+    }
+  }
+  
+  /**********************************************************/
+  /******** Démon **************/
+  /**********************************************************/
+  class Demon extends Enemy {
+    constructor() {
+      super(65, "img/imEnemy/Demon.svg", "Trident", "tranchant", 14);
+    }
+  
+    attaquer(cible) {
+      super.attaquer(cible);
+    }
+  }
+  
+  /**********************************************************/
+  /******** Chien à Trois Têtes **************/
+  /**********************************************************/
+  class ChienATroisTete extends Enemy {
+    constructor() {
+      super(70, "img/imEnemy/ChienATroisTete.svg", "Crocs", "tranchant", 16);
+    }
+  
+    attaquer(cible) {
+      super.attaquer(cible);
+    }
+  }
+  
+  /**********************************************************/
+  /******** Chevalier Corrompu **************/
+  /**********************************************************/
+  class ChevalierCorrompu extends Enemy {
+    constructor() {
+      super(75, "img/imEnemy/ChevalierCorrompu.svg", "Épée maudite", "critique", 18);
+    }
+  
+    attaquer(cible) {
+      super.attaquer(cible);
+    }
+  }
--- main3.js
+++ main3.js
@@ -46,3 +46,110 @@ const acheter = () => {
 } else {
   console.log("Aucun héros sélectionné.");
 }
+
+
+
+
+
+function jouerPartie() {
+  const hero = new Guerrier();
+  const marchand = new Marchand();
+  let phase = 1;
+  let enemyIndex = 0;
+  const enemyList = [
+    new Loup(),
+    new ZombieAffamer(),
+    new Zombie(),
+    new Squelette(),
+    new PetitDragon(),
+    new Ours(),
+    new Minotor(),
+    new MasterDragon(),
+    new Hydre(),
+    new GuerrierOrc(),
+    new GrosZombie(),
+    new Gorille(),
+    new Golem(),
+    new Fantome(),
+    new DragonRouge(),
+    new DragonBleu(),
+    new Demon(),
+    new ChienATroisTete(),
+    new ChevalierCorrompu()
+  ];
+
+  // Fonction pour attaquer
+  const attaquer = () => {
+    const currentEnemy = enemyList[enemyIndex];
+
+    if (currentEnemy && currentEnemy.ptnVie > 0) {
+      currentEnemy.ptnVie -= hero.degats;
+      console.log(`Vous attaquez ${currentEnemy.constructor.name} !`);
+      console.log(`Dégâts infligés: ${hero.degats}`);
+      console.log(`Points de vie restants: ${currentEnemy.ptnVie}`);
+
+      if (currentEnemy.ptnVie <= 0) {
+        console.log(`${currentEnemy.constructor.name} est vaincu !`);
+        enemyIndex++;
+        if (enemyIndex >= enemyList.length) {
+          console.log("Vous avez vaincu tous les ennemis !");
+          return;
+        }
+        const nextEnemy = enemyList[enemyIndex];
+        console.log(`Prochain ennemi: ${nextEnemy.constructor.name}`);
+        nextEnemy.afficher(pvEnemyElement, degatsEnemyElement, armeEnemyElement, faiblesseEnemyElement);
+      } else {
+        enemyAttaque(currentEnemy, hero);
+      }
+    } else {
+      console.log("Il n'y a pas d'ennemi à attaquer !");
+    }
+  };
+
+  // Fonction pour l'attaque de l'ennemi
+  const enemyAttaque = (enemy, hero) => {
+    hero.ptnVie -= enemy.degats;
+    console.log(`${enemy.constructor.name} attaque ${hero.constructor.name} !`);
+    console.log(`Dégâts infligés: ${enemy.degats}`);
+    console.log(`Points de vie restants: ${hero.ptnVie}`);
+
+    if (hero.ptnVie <= 0) {
+      console.log(`${hero.constructor.name} est mort ! Partie terminée.`);
+      return;
+    }
+  };
+
+  // Fonction pour acheter
+  const acheter = () => {
+    const magasin = marchand.getMagasin(hero.constructor);
+    // Afficher le magasin dans l'interface utilisateur et gérer l'achat
+  };
+
+  // Mettre à jour l'interface utilisateur
+  const mettreAJourInterface = () => {
+    hero.afficher(ptnVieElement, potionSoinElement, armeElement, goldElement);
+    const currentEnemy = enemyList[enemyIndex];
+    if (currentEnemy) {
+      currentEnemy.afficher(pvEnemyElement, degatsEnemyElement, armeEnemyElement, faiblesseEnemyElement);
+    }
+  };
+
+  // Gérer les événements de clic sur les boutons
+  buttonAttack.addEventListener('click', attaquer);
+  buttonAchat.addEventListener('click', acheter);
+
+  // Initialiser l'interface utilisateur
+  mettreAJourInterface();
+
+  while (enemyIndex < enemyList.length && hero.ptnVie > 0) {
+    console.log(`Phase ${phase}`);
+    const currentEnemy = enemyList[enemyIndex];
+    console.log(`Prochain ennemi: ${currentEnemy.constructor.name}`);
+    currentEnemy.afficher(pvEnemyElement, degatsEnemyElement, armeEnemyElement, faiblesseEnemyElement);
+
+    // Attente de l'action du joueur (attaque ou achat)
+    // Mettez ici la logique pour gérer les actions du joueur et les mises à jour de l'interface utilisateur
+
+    phase++;
+  }
+}
