a78769e9b98b0a9d2111baba1cb1bf668ac24b58
Dos Santos Davide <dos.davide91@gmail.com>
2023-06-07 11:05:35 +0200

    commit

--- main3.js
+++ main3.js
@@ -34,24 +34,24 @@ function jouerPartie() {
   
    const enemyList = [
      new Loup(20, "img/imgEnemy/Loup.svg", "Croc", "magic", 5),
-     new ZombieAffamer(40,"img/imgEnemy/ZombieAffamer.svg","Griffes","tranchant",10),
-     new Zombie(35,"img/imgEnemy/Zombie.svg","Mains","trnahcant",8),
      new Squelette(25,"img/imgEnemy/Squelette.svg","Épée","critique",6),
-     new PetitDragon(50,"img/imgEnemy/PetitDragon.svg","Feu", "critique",12),
+     new Fantome( 30,"img/imgEnemy/Fantome.svg","Ectoplasme","magic",8),
+     new Zombie(35,"img/imgEnemy/Zombie.svg","Mains","trnahcant",8),
+     new ZombieAffamer(40,"img/imgEnemy/ZombieAffamer.svg","Griffes","tranchant",10),
      new Ours(45, "img/imgEnemy/Ours.svg", "Griffes", "Feu", 10),
+     new PetitDragon(50,"img/imgEnemy/PetitDragon.svg","Feu", "critique",12),
      new Minotor(55,"img/imgEnemy/Minotor.svg","Hache","critique",14),
-     new MasterDragon( 100,"img/imgEnemy/MasterDragon.svg","Feu","Glace",25),
-     new Hydre(75, "img/imgEnemy/Hydre.svg", "Crocs venimeux", "Feu", 16),
      new GuerrierOrc(55,"img/imgEnemy/GuerrierOrc.svg","Hache","critique",12),
      new GrosZombie(60,"img/imgEnemy/GrosZombie.svg","Mains","tranchant",14),
      new Gorille(50,"img/imgEnemy/Gorille.svg","Poings","critique",12),
+     new Demon(65,"img/imgEnemy/Demon.svg","Trident de l'enfer","tranchant",14),
      new Golem(65, "img/imgEnemy/Golem.svg", "Massue", "magic", 15),
-     new Fantome( 30,"img/imgEnemy/Fantome.svg","Ectoplasme","magic",8),
+     new Hydre(75, "img/imgEnemy/Hydre.svg", "Crocs venimeux", "tranchant", 16),
+     new ChienATroisTete(70,"img/imgEnemy/ChienTroisTete.svg","Crocs","tranchant",16),
+     new ChevalierCorrompu( 75,"img/imgEnemy/ChevalierCoronpu.svg","Épée maudite","critique",18),
      new DragonRouge( 90,"img/imgEnemy/DragonRouge.svg","Souffle de feu","critique",20),
      new DragonBleu(85, "img/imgEnemy/DragonBleu.svg", "Souffle de glace", "critique", 18),
-     new Demon(65,"img/imgEnemy/Demon.svg","Trident de l'enfer","tranchant",14),
-     new ChienATroisTete(70,"img/imgEnemy/ChienATroisTete.svg","Crocs","tranchant",16),
-     new ChevalierCorrompu( 75,"img/imgEnemy/ChevalierCorrompu.svg","Épée maudite","critique",18)
+     new MasterDragon( 100,"img/imgEnemy/MasterDragon.svg","Feu","Glace",25),
    ];
   
   // Vérifier si un héros a été sélectionné
