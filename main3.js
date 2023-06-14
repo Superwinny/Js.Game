2a619756b9a61e5daa984e07c01ab792bf006425
Dos Santos Davide <dos.davide91@gmail.com>
2023-06-14 15:44:45 +0200

    commit

--- main3.js
+++ main3.js
@@ -1,3 +1,5 @@
+//Sélectionner la div du message
+const messageContainer = document.getElementById("message-container");
 
 // Déclaration des variables globales
 let selectedImageElement = document.getElementById('selectedImage');
@@ -29,43 +31,94 @@ function getEnemy(enemyList,index){
 // Déclaration d'une variable pour suivre l'état du clignotement
 let isFlashing = false;
 
-// Fonction pour clignoter l'image de l'ennemi
-async function flashEnemyImage() {
+function flashEnemyImage() {
   if (isFlashing) {
-    return; // Empêcher le clignotement si le clignotement est déjà en cours
+    return Promise.resolve(); // Renvoyer une promesse résolue si le clignotement est déjà en cours
   }
 
   isFlashing = true;
   let visible = true;
-  const flashInterval = setInterval(() => {
-    if (visible) {
-      selectedImageEnemy.style.opacity = 0.2; 
-    } else {
-      selectedImageEnemy.style.opacity = 1; 
-    }
-    visible = !visible;
-  }, 300); // Temps en millisecondes pour chaque clignotement 
+  selectedImageEnemy.style.opacity = 1; // Réinitialiser l'opacité de l'image
 
-  // Arrêter le clignotement après un certain délai 
-  await setTimeout(() => {
-    clearInterval(flashInterval); // Arrêter l'intervalle de clignotement
-    selectedImageEnemy.style.opacity = 1; // Rétablir l'opacité de l'image au cas où elle serait réduite
-    isFlashing = false; // Réinitialiser l'état du clignotement
-  }, 2000); // Temps en millisecondes avant d'arrêter le clignotement 
+  return new Promise((resolve, reject) => {
+    const flashInterval = setInterval(() => {
+      if (visible) {
+        selectedImageEnemy.style.opacity = 0.2;
+      } else {
+        selectedImageEnemy.style.opacity = 1;
+      }
+      visible = !visible;
+    }, 300); // Temps en millisecondes pour chaque clignotement
+
+    setTimeout(() => {
+      clearInterval(flashInterval); // Arrêter l'intervalle de clignotement
+      selectedImageEnemy.style.opacity = 1; // Rétablir l'opacité de l'image au cas où elle serait réduite
+      isFlashing = false; // Réinitialiser l'état du clignotement
+      resolve(); // Résoudre la promesse une fois le clignotement terminé
+    }, 2000); // Temps en millisecondes avant d'arrêter le clignotement
+  });
 }
 function flashEnemyImageMort() {
-  return 
-  let opacity = 1;
-  const flashIntervalMort = setInterval(() => {
-    opacity -= 0.1; // Réduire l'opacité de 0.1 à chaque intervalle 
-    selectedImageEnemy.style.opacity = opacity;
+  if (isFlashing) {
+    return Promise.resolve(); // Renvoyer une promesse résolue si le clignotement est déjà en cours
+  }
 
-    if (opacity <= 0) {
-      clearInterval(flashIntervalMort); // Arrêter l'intervalle lorsque l'opacité atteint 0
-      selectedImageEnemy.style.display = "none"; // Masquer complètement l'image
-    }
-  }, 100); // Temps en millisecondes pour chaque intervalle 
+  isFlashing = true;
+  selectedImageEnemy.style.opacity = 1; // Réinitialiser l'opacité de l'ennemi
+
+  return new Promise((resolve, reject) => {
+    let opacity = 1;
+
+    const flashInterval = setInterval(() => {
+      opacity -= 0.1; // Réduire l'opacité de 0.1 à chaque intervalle
+      selectedImageEnemy.style.opacity = opacity;
+
+      if (opacity <= 0) {
+        clearInterval(flashInterval); // Arrêter l'intervalle lorsque l'opacité atteint 0
+        selectedImageEnemy.style.display = "none"; // Masquer complètement l'image
+        selectedImageEnemy.style.opacity = 1; // Rétablir l'opacité pour le prochain ennemi
+        isFlashing = false; // Réinitialiser l'état du clignotement
+        resolve(); // Résoudre la promesse une fois l'animation terminée
+      }
+    }, 100); // Temps en millisecondes pour chaque intervalle
+
+    setTimeout(() => {
+      clearInterval(flashInterval); // Arrêter l'intervalle de clignotement
+      selectedImageEnemy.style.display = "block"; // Rétablir l'affichage de l'image
+      selectedImageEnemy.style.opacity = 1; // Rétablir l'opacité de l'image pour le prochain ennemi
+      isFlashing = false; // Réinitialiser l'état du clignotement
+      resolve(); // Résoudre la promesse une fois le clignotement terminé
+    }, 1000); // Temps en millisecondes avant d'arrêter le clignotement
+  });
 }
+async function flashHeroImage() {
+  if (isFlashing) {
+    return Promise.resolve();
+  }
+
+  isFlashing = true;
+  let visible = true;
+  selectedImageElement.style.opacity = 1;
+
+  return new Promise((resolve, reject) => {
+    const flashInterval = setInterval(() => {
+      if (visible) {
+        selectedImageElement.style.opacity = 0.2;
+      } else {
+        selectedImageElement.style.opacity = 1;
+      }
+      visible = !visible;
+    }, 300);
+
+    setTimeout(() => {
+      clearInterval(flashInterval);
+      selectedImageElement.style.opacity = 1;
+      isFlashing = false;
+      resolve();
+    }, 2000);
+  });
+}
+
 
 function jouerPartie() {
    
@@ -74,6 +127,7 @@ function jouerPartie() {
     const marchand = new Marchand();
     let round = 0 // TODO: get from local storage
     
+
   let enemyIndex = 0;
   
    const enemyList = [
@@ -99,82 +153,74 @@ function jouerPartie() {
    ];
   
   // Vérifier si un héros a été sélectionné
- if (hero) {
-   if(!enemy){
-    enemy = getEnemy(enemyList, round)
-    enemy.afficher(pvEnemyElement, degatsEnemyElement,armeEnemyElement,faiblesseEnemyElement,selectedImageEnemy)
-   }
- } else {
-  // Rediriger sur le premier index 
-   window.location.href = "index.html";
- }
+  if (hero) {
+    if (!enemy) {
+      enemy = getEnemy(enemyList, round);
+      enemy.afficher(pvEnemyElement, degatsEnemyElement, armeEnemyElement, faiblesseEnemyElement, selectedImageEnemy);
+    }
+  } else {
+    // Rediriger sur le premier index
+    window.location.href = "index.html";
+  }
 
- mettreAJourInterface(hero, enemy)
+  mettreAJourInterface(hero, enemy);
 
   // Fonction pour l'attaque de l'ennemi
-  const enemyAttaque = (enemy, hero) => {
+  const enemyAttaque = async (enemy, hero) => {
     hero.ptnVie -= enemy.degats;
-    console.log(`${enemy.constructor.name} attaque ${hero.constructor.name} !`);
-    console.log(`Dégâts infligés: ${enemy.degats}`);
-    console.log(`Points de vie restants: ${hero.ptnVie}`);
-
+    await flashHeroImage();
+  
+    const message1 = `${enemy.constructor.name} attaque ${hero.constructor.name} !`;
+    const message2 = `Dégâts infligés: ${enemy.degats}`;
+    const message3 = `Points de vie restants: ${hero.ptnVie}`;
+  
+    messageContainer.innerHTML += `<p>${message1}</p>`;
+    messageContainer.innerHTML += `<p>${message2}</p>`;
+    messageContainer.innerHTML += `<p>${message3}</p>`;
+  
     if (hero.ptnVie <= 0) {
       console.log(`${hero.constructor.name} est mort ! Partie terminée.`);
       return;
     }
-   
   };
   
-  
-  
+
   // Fonction pour attaquer
-  const attaquer = () => {
-   
+  const attaquer = async () => {
     if (enemy && enemy.ptnVie > 0) {
-      
       hero.attaquer(enemy);
-      flashEnemyImage();
-  
+      await flashEnemyImage();
+
       if (enemy.ptnVie <= 0) {
-        
         console.log(`${enemy.constructor.name} est vaincu !`);
-        flashEnemyImageMort()
         enemyIndex++;
-  
+        await flashEnemyImageMort();
+      
         if (enemyIndex >= enemyList.length) {
           console.log("Vous avez vaincu tous les ennemis !");
           return;
         }
-  
+      
         enemy = getEnemy(enemyList, ++round);
         mettreAJourInterface(hero, enemy);
-  
-       
-  
-        if (enemyIndex > 0 && enemyIndex % 5 === 0) {
+      
+        if (enemyIndex > 0 && enemyIndex % 5 === 0 && Math.random() <= 0.5) {
           console.log("Le marchand apparaît !");
           enemy.setEnemyIntoLocalStorage();
           hero.setHeroIntoLocalStorage();
-          mettreAJourInterface(hero, enemy);
           window.location.href = "marchand.html";
         }
-       
       } else {
-      
         enemyAttaque(enemy, hero);
-        
       }
-  
+
       mettreAJourInterface(hero, enemy);
     } else {
-      
       console.log("Il n'y a pas d'ennemi à attaquer !");
     }
   };
- 
-  buttonAttack.addEventListener("click", attaquer);
 
-  
+  buttonAttack.addEventListener("click", attaquer);
 }
 
- jouerPartie()
+jouerPartie();
